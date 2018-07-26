0eb90870ea1e4ddf90720aded7f5a6ca22bd41d5
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 18:28:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def create
-    user = User.new(user_params).save
+    user = User.new(user_params)
+    binding.pry
     session[:user_id] = user.id
     redirect_to users_path
   end
