e5c9b97b26cdc8fdb8490be016fce0049076f6d7
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 18:24:12 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
   def create
+
   end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end 
 
 end
