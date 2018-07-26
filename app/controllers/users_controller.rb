a64004151a5f0b59cfe2dd85aac58e8cfe837365
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 18:30:43 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
 
   def create
-    user = User.new(user_params)
-    user.save
-    session[:user_id] = user.id
-    redirect_to users_path
+    if params[:user][:password] != params[:user][:password_confirmation]
+      redirect_to new_user_path 
+    else 
+      user = User.new(user_params)
+      user.save
+      session[:user_id] = user.id
+      redirect_to users_path
+    end
   end
 
   def show
