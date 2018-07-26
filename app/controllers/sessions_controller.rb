f2bc3e9e774120d60e6566e6d69aeb2951b1229f
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 18:22:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params["user"]["name"])
     session[:user_id] = @user.id if  @user.authenticate(params["user"]["password"])
+    if session[:user_id]
+      redirect_to users_path
+    else
+      redirect_to login_path
+    end
   end
 
 end
