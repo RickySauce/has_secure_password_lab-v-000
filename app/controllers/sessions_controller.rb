539ec405957ed5291ae0fce69531f9d0bac761a8
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 18:21:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params["user"]["name"])
-    session[:user_id] = @user.id if params["user"]["password"] = @user.authenticate(:password)
+    session[:user_id] = @user.id if  @user.authenticate(params["user"]["password"])
   end
 
 end
